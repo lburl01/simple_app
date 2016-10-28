f654d772bd5b0693ffe77d335916c16d130b4e75
Lori Burleson <lori.burleson@gmail.com>
2016-10-28 11:45:23 -0400

    Changed logged in root to be the search page.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+#
+class HomeController < ApplicationController
+  def index
+  end
+
+  def search
+    # parameters = { term: params[:term], limit: 16 }
+    render json: Yelp.client.search('San Francisco')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root 'home#index', as: :authenticated_root
+  end
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  post '/search' => 'home#search'
 end
